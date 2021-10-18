9adfac2be6bbd821170c75e22656ffc202568d8b
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-10-18 10:28:26 +0300

    Update * and ** syntax.py

--- 10 writing effectinve functions/* and ** syntax.py
+++ 10 writing effectinve functions/* and ** syntax.py
@@ -1,6 +1,12 @@
 test_list = [1, 2, 3, 4]
+# <editor-fold desc="simple print">
+print('\nsimple print\n')
 print('test_list', test_list)
 print('*test_list', *test_list)
+# </editor-fold>
+
+# <editor-fold desc="test_func">
+print('\ntest_func\n')
 
 
 def test_func(some_list):
@@ -8,9 +14,10 @@ def test_func(some_list):
 
 
 test_func(test_list)
-# _________________________________
-print('\nnew stuff\n')
-# variadic functions
+# </editor-fold>
+
+# <editor-fold desc="variadic functions">
+print('\nvariadic functions\n')
 
 
 def sum_it(*args):
@@ -20,28 +27,17 @@ def sum_it(*args):
     print(result)
 
 
+sum_it(test_list)
 sum_it(1, 1)
 sum_it(1, 2, 3, 4, 5)
 
-
+"""
 def sum_it(*test_list):
     result = 0
 
     result += test_list
     print(result)
 
-"""print('\nnew stuff \n')
 
-
-def append_func(col_values_list):
-    modified_values_list = []
-    for value in col_values_list:
-        if value % 2 == 0:
-            modified_values_list.append(value)
-    print(modified_values_list)
-    return modified_values_list
-
-
-append_func(test_list)"""
-
-sum_it()
+sum_it()"""
+# </editor-fold>
