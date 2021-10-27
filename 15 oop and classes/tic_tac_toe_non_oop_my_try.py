ff48a1ac0ece38117fc489d4e11c2af605414ff1
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-10-27 23:48:53 +0300

    Update tic_tac_toe_non_oop_my_try.py

--- 15 oop and classes/tic_tac_toe_non_oop_my_try.py
+++ 15 oop and classes/tic_tac_toe_non_oop_my_try.py
@@ -14,12 +14,13 @@ def update_board(board, move, player):
 
 
 def print_board(board):
+    b = board
     print(f'''
-        {board['1']}|{board['2']}|{board['3']}      1 2 3
+        {b['1']}|{b['2']}|{b['3']}      1 2 3
         -----
-        {board['4']}|{board['5']}|{board['6']}      4 5 6
+        {b['4']}|{b['5']}|{b['6']}      4 5 6
         -----
-        {board['7']}|{board['8']}|{board['9']}      7 8 9
+        {b['7']}|{b['8']}|{b['9']}      7 8 9
         ''')
 
 
@@ -30,29 +31,18 @@ def move_is_valid(board, move):
     return True
 
 
-def there_is_winner(board):
-    b = board
-    if      b['1'] == b['2'] == b['3'] == 'X' or \
-            b['4'] == b['5'] == b['6'] == 'X' or \
-            b['7'] == b['8'] == b['9'] == 'X' or \
-            b['1'] == b['5'] == b['9'] == 'X' or \
-            b['7'] == b['5'] == b['3'] == 'X' or \
-            b['1'] == b['4'] == b['7'] == 'X' or \
-            b['2'] == b['5'] == b['8'] == 'X' or \
-            b['3'] == b['6'] == b['9'] == 'X':
+def there_is_winner(board, player):
+    b, p = board, player
+    if      b['1'] == b['2'] == b['3'] == p or \
+            b['4'] == b['5'] == b['6'] == p or \
+            b['7'] == b['8'] == b['9'] == p or \
+            b['1'] == b['5'] == b['9'] == p or \
+            b['7'] == b['5'] == b['3'] == p or \
+            b['1'] == b['4'] == b['7'] == p or \
+            b['2'] == b['5'] == b['8'] == p or \
+            b['3'] == b['6'] == b['9'] == p:
         print_board(board)
-        print('X is winner')
-        return True
-    if      b['1'] == b['2'] == b['3'] == 'O' or \
-            b['4'] == b['5'] == b['6'] == 'O' or \
-            b['7'] == b['8'] == b['9'] == 'O' or \
-            b['1'] == b['5'] == b['9'] == 'O' or \
-            b['7'] == b['5'] == b['3'] == 'O' or \
-            b['1'] == b['4'] == b['7'] == 'O' or \
-            b['2'] == b['5'] == b['8'] == 'O' or \
-            b['3'] == b['6'] == b['9'] == 'O':
-        print_board(board)
-        print('O is winner')
+        print(f'{p} is winner')
         return True
     return False
 
@@ -71,7 +61,7 @@ while there_are_free_cells():
     move = input(f'player {current_player} enter your move: ')
     if move_is_valid(board, move):
         update_board(board, move, current_player)
-        if there_is_winner(board):
+        if there_is_winner(board, current_player):
             exit()
         print_board(board)
         current_player, next_player = next_player, current_player
