225d92cda2baa7becb89b87f43edf7ceb1e899bb
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-11-01 10:33:42 +0200

    Update inheritance_eg.py

--- 16 oop and inheritance/inheritance_eg.py
+++ 16 oop and inheritance/inheritance_eg.py
@@ -1,36 +1,33 @@
 class ParentClass:
     attr = 'abc'
 
-    def print_hello(self):
+    def parent_method(self):
         print('Hello world')
 
 
 class ChildClass(ParentClass):
-    def some_new_method(self):
+    """def parent_method(self):
+        print('Hello world from child class')"""
+
+    def child_method(self):
         print('ParentClass objects don"t have this method.')
 
 
 class GrandChildClass(ChildClass):
-    def another_new_method(self):
+    def grand_child_method(self):
         print('Only GrandchildClass objects have this method.')
 
 
-print('Create a ParentClass object and call its methods:')
+print('\nCreate a ParentClass object and call its methods:')
 parent = ParentClass()
-parent.print_hello()
-print()
 
-print('Create a ChildClass object and call its methods:')
+
+print('\nCreate a ChildClass object and call its methods:')
 child = ChildClass()
-child.print_hello()
-child.some_new_method()
-print()
 
-print('Create a GrandchildClass object and call its methods:')
+
+print('\nCreate a GrandchildClass object and call its methods:')
 grand_child = GrandChildClass()
-grand_child.print_hello()
-grand_child.some_new_method()
-grand_child.another_new_method()
 
-print(ParentClass.attr)
-print(ChildClass.attr)
+
+
