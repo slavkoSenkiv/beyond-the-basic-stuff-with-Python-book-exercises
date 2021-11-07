c5796aba548693bc477338c6032a71c32c857dff
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-11-07 10:41:13 +0200

    upd

--- 16 oop and inheritance/composition/vehicles_composition.py
+++ 16 oop and inheritance/composition/vehicles_composition.py
@@ -1,3 +1,5 @@
+# inheritance
+"""
 class Vehicle:
     def __init__(self):
         print('Vehicle created')
@@ -13,12 +15,57 @@ class Car(Vehicle):
     def __init__(self):
         print('Car created')
 
-class Mororcycle(Vehicle):
+
+class Motorcycle(Vehicle):
     def __init__(self):
-        print('Mororcycle created')
+        print('Motorcycle created')
+
 
 class LunarRover(Vehicle):
     def __init__(self):
         print('LunarRover created')
 
+
+vehicle = Vehicle()
+car = Car()
+motor_cycle = Motorcycle()
+lunar_rover = LunarRover()"""
+
+
+# composition
+class CombustionEngine:
+    def __init__(self):
+        print('Combustion engine created')
+
+
+class Vehicle:
+    def __init__(self):
+        print('Vehicle created')
+
+    def stat_ignition(self):
+        pass
+
+    def change_tire(self):
+        pass
+
+
+class Car(Vehicle):
+    def __init__(self):
+        print('Car created')
+
+
+class Motorcycle(Vehicle):
+    def __init__(self):
+        print('Motorcycle created')
+
+
+class LunarRover(Vehicle):
+    def __init__(self):
+        print('LunarRover created')
+
+
+vehicle = Vehicle()
+car = Car()
+motor_cycle = Motorcycle()
+lunar_rover = LunarRover()
 
